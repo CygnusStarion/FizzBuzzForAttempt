f76a775c0022fb27443db96356a0769888511bd8
CygnusStarion <54466514+CygnusStarion@users.noreply.github.com>
2020-03-04 06:46:17 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+
+for (i=0; i<=100; i++); {
+    console.log("FizzBuzz!");
+    console.log(i);
+
+    if (!(i % 3) && !(i % 5)) {
+        console.log("FizzBuzz!");
+      } else if (i % 3 === 0) {
+      } else if (!(i % 3)) {
+        console.log("Fizz!");
+      } else if (i % 5 === 0) {
+      } else if (!(i % 5)) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
+
+
+}
+
+
+
